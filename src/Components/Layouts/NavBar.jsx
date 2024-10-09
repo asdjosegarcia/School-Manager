b5def3c11f30dc7fd0b1ec777ff9847ab6cd0794
asdjosegarcia <asdjosegarcia@gmail.com>
2024-10-09 20:15:10 -0300

    style: add desktop-style to navbar

--- src/Components/Layouts/NavBar.jsx
+++ src/Components/Layouts/NavBar.jsx
@@ -7,25 +7,27 @@ import { useNavigate } from 'react-router-dom';
 
 
 const NavBar = (props) => {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
     const [getMenu, setMenu] = useState(false);
 
     return (
         <>
             <div className='NavBar'>
-                <button className='button' onClick={()=>{setMenu(true)}} >
-                    <img src="/icons/menu.svg" alt="" />
-                </button>
-                <h1 className='title'>
-                    IES 0000
-                </h1>
-                <button className='button' onClick={() => navigate('/notifications')}>
-                    <span className='notification-span'>{'9+'}</span>
-                    <img src="/icons/bell.svg" alt="" />
-                </button>
+                <div className='container'>
+                    <button className='button' onClick={() => { setMenu(true) }} >
+                        <img src="/icons/menu.svg" alt="" />
+                    </button>
+                    <h1 className='title'>
+                        IES 0000
+                    </h1>
+                    <button className='button' onClick={() => navigate('/notifications')}>
+                        <span className='notification-span'>{'9+'}</span>
+                        <img src="/icons/bell.svg" alt="" />
+                    </button>
 
+                </div>
             </div>
-             <Menu getMenu={getMenu} setMenu={setMenu}/> 
+            <Menu getMenu={getMenu} setMenu={setMenu} />
         </>
     )
 }
