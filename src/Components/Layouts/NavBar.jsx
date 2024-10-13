e5661ae41abb79c3949dd25049ceb0065a2c9a67
asdjosegarcia <asdjosegarcia@gmail.com>
2024-10-13 00:55:26 -0300

    style: change NavBar sktop style feat: add desktop compatibility to ExamsPage feat: add desktop compatibility to Menu

--- src/Components/Layouts/NavBar.jsx
+++ src/Components/Layouts/NavBar.jsx
@@ -12,22 +12,22 @@ const NavBar = (props) => {
 
     return (
         <>
-            <div className='NavBar'>
+            <div className='NavBar'>    
                 <div className='container'>
-                    <button className='button' onClick={() => { setMenu(true) }} >
+                    <button className='button menu-button' onClick={() => { setMenu(true) }} >
                         <img src="/icons/menu.svg" alt="" />
                     </button>
                     <h1 className='title'>
                         IES 0000
                     </h1>
-                    <button className='button' onClick={() => navigate('/notifications')}>
+                    <button className='button notification-button' onClick={() => navigate('/notifications')}>
                         <span className='notification-span'>{'9+'}</span>
                         <img src="/icons/bell.svg" alt="" />
                     </button>
 
                 </div>
-            </div>
             <Menu getMenu={getMenu} setMenu={setMenu} />
+            </div>
         </>
     )
 }
